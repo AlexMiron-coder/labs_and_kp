8052a738f3704faa910c97dc8a525cf45dfcdc3a
AlexMiron-coder <57828189+AlexMiron-coder@users.noreply.github.com>
2020-08-28 21:29:52 +0300

    Update deque.c

--- lab25-26/deque.c
+++ lab25-26/deque.c
@@ -40,54 +40,36 @@ int* push_front(int add, int *deque, int *size){
     return deque;
 }
 
-/*int *push_front(int add, int *deque, int *size){
-    *size += 1;
-    if(!deque[*size]){
-        deque = (int *)realloc(deque, *size * (sizeof(int)));
-    }
-    for(int i = *size - 1; i > 0; i--) deque[i] = deque[i-1];
-    deque[0] = add;
-}*/
-
 int* pop_front(int *deque, int *size){
     if(*size == 0){
         printf("\n Deque is empty\n");
     }
     else if(*size == 1){
-        //int out = deque[0];
         free(deque);
         *size = 0;
-        //return out;
     }
     else{
-        //int out = deque[0];
+        
         for(int i = 0; i < *size - 1; i++){
             deque[i] = deque[i+1];
         }
         *size -= 1;
         deque = (int *)realloc(deque, *size*(sizeof(int)));
-        //return out;
     }
     return deque;
 }
 
 int* pop_back(int *deque, int *size){
     if(*size == 0){
-        ///int out = -100000;
         printf("\n Deque is empty\n");
-        //return out;
     }
     else if(*size == 1){
-        //int out = deque[0];
         free(deque);
         *size = 0;
-        //return out;
     }
     else{
-        //int out = deque[*size-1];
         *size -= 1;
         deque = (int *)realloc(deque, *size*(sizeof(int)));
-        //return out;
     }
     return deque;
 }
