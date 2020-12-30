9c1b5ecea51df3044b99476c686166a34055a998
AlexMiron-coder <57828189+AlexMiron-coder@users.noreply.github.com>
2020-12-30 19:58:04 +0300

    Add files via upload

---
+++ lab_1.2_OOP/lab_1.2_OOP/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+
+     //      A 
+     //   /  |  \
+     //  B   C   J
+     // /   /|\
+     //D---E F K
+
+namespace lab_1._2_OOP
+{
+
+    class A
+    {
+        B objB = null;
+        public B b
+        {
+            get { return objB; } //read
+            
+        }
+        C objC = null;
+        public C c
+        {
+            get { return objC; }
+            
+        }
+        J objJ = null;
+        public J j
+        {
+            get { return objJ; }
+           
+        }
+        public A(B b, C c, J j)
+        {
+            objB = b;
+            objC = c;
+            objJ = j; 
+        }
+        public void methA()
+        {
+            Console.WriteLine("Method A, hash = {0}", this.GetHashCode());
+        }
+
+    }
+    class B
+    {
+        D objD = null;
+        public B(D d)
+        {
+            objD = d;
+        }
+        public D d { get { return objD; } }
+        public void methB()
+        {
+            Console.WriteLine("Method B, hash = {0}", this.GetHashCode());
+        }
+    }
+    class C
+    {
+        E objE = null;
+        F objF = null;
+        K objK = null;
+        public C(E e, F f, K k)
+        {
+            objE = e;
+            objF = f;
+            objK = k;
+        }
+        public E e { get { return objE; } }
+        public F f { get { return objF; } }
+        public K k { get { return objK; } }
+        public void methC()
+        {
+            Console.WriteLine("Method C, hash = {0}", this.GetHashCode());
+        }
+    }
+    class J
+    {
+        public int vJ;
+        public J(int val) { vJ = val; }
+        public J() { }
+        public void methJ()
+        {
+            Console.WriteLine("Method J, hash = {0}", this.GetHashCode());
+        }
+    }
+    class D
+    {
+        public int vD;
+        public D(int val) { vD = val; }
+        public D() { }
+        public void methD()
+        {
+            Console.WriteLine("Method D, hash = {0}", this.GetHashCode());
+        }
+    }
+    class E
+    {
+        public int vE;
+        public E(int val) { vE = val; }
+        public E() { }
+        public void methE()
+        {
+            Console.WriteLine("Method E, hash = {0}", this.GetHashCode());
+        }
+    }
+    class F
+    {
+        public int vF;
+        public F(int val) { vF = val; }
+        public F() { }
+        public void methF()
+        {
+            Console.WriteLine("Method F, hash = {0}", this.GetHashCode());
+        }
+    }
+    class K
+    {
+        public int vK;
+        public K(int val) { vK = val; }
+        public K() { }
+        public void methK()
+        {
+            Console.WriteLine("Method K, hash = {0}", this.GetHashCode());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            K objK = new K();
+            D objD = new D();
+            E objE = new E();
+            F objF = new F();
+            //----------------------------------------------
+            J objJ = new J();
+            //----------------------------------------------
+            B objB = new B(objD);
+            C objC = new C(objE, objF, objK);
+            //----------------------------------------------
+            A objA = new A(objB, objC, objJ);
+            //----------------------------------------------
+            objA.methA();
+
+            B b = objA.b;
+            C c = objA.c;
+            J j = objA.j;
+
+            objB.methB();
+            b.methB();
+            //----------------------------------------------
+            D d = b.d;
+            objD.methD();
+            d.methD();
+            //----------------------------------------------
+            objA.c.e.methE();
+            objE.methE();
+            //----------------------------------------------
+            objC.methC();
+            objA.c.methC();
+            //----------------------------------------------
+            objF.methF();
+            objA.c.f.methF();
+            //----------------------------------------------
+            objJ.methJ();
+            objA.j.methJ();
+        }
+    }
+}
---
+++ lab_2.1_OOP/lab_2.1_OOP/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+
+namespace lab_2._1_OOP
+{
+    class A
+    {
+        B b = null;
+        C c = null;
+        J j = null;
+        //public A(B b, C c, J j)
+        //{
+        //    this.b = b;
+        //    this.c = c;
+        //    this.j = j;
+        //}
+        public A()
+        {
+            b = new B();
+            c = new C();
+            j = new J();
+        }
+        public B objb { get { return b; } }
+        public C objc { get { return c; } }
+        public J objj { get { return j; } }
+        public void m_a()
+        {
+            Console.WriteLine("Method A, object hash = {0}", this.GetHashCode());
+        }
+    }
+    class B
+    {
+        D d = null;
+        public B(D d)
+        {
+            this.d = d;
+        }
+        public B()
+        {
+            d = new D();
+        }
+        public D objd { get { return d; } }
+        public void m_b()
+        {
+            Console.WriteLine("Method B, object hash = {0}", this.GetHashCode());
+        }
+    }
+    class C
+    {
+        E e = null;
+        F f = null;
+        K k = null;
+        public C(E e, F f, K k)
+        {
+            this.e = e;
+            this.f = f;
+            this.k = k;
+        }
+        public C()
+        {
+            e = new E();
+            f = new F();
+            k = new K();
+        }
+        public E obje { get { return e; } }
+        public F objf { get { return f; } }
+        public K objk { get { return k; } }
+        public void m_c()
+        {
+            Console.WriteLine("Method C, object hash = {0}", this.GetHashCode());
+        }
+    }
+    class J
+    {
+        int j = 0;
+        public int ij { get { return j; } }
+        public J() { }
+        public J(int j)
+        {
+            this.j = j;
+        }
+        public void m_j()
+        {
+            Console.WriteLine("Method J, object hash = {0}", this.GetHashCode());
+        }
+    }
+    class D
+    {
+        int d = 0;
+        public int id { get { return d; } }
+        public D() { }
+        public D(int d)
+        {
+            this.d = d;
+        }
+        public void m_d()
+        {
+            Console.WriteLine("Method D, object hash = {0}", this.GetHashCode());
+        }
+    }
+    class E
+    {
+        int e = 0;
+        public int ie { get { return e; } }
+        public E() { }
+        public E(int e)
+        {
+            this.e = e;
+        }
+        public void m_e()
+        {
+            Console.WriteLine("Method E, object hash = {0}", this.GetHashCode());
+
+        }
+    }
+    class F
+    {
+        int f = 0;
+        public int fi { get { return f; } }
+        public F() { }
+        public F(int f)
+        {
+            this.f = f;
+        }
+        public void m_f()
+        {
+            Console.WriteLine("Method F, object hash = {0}", this.GetHashCode());
+        }
+    }
+    class K
+    {
+        int k = 0;
+        public int ik { get { return k; } }
+        public K() { }
+        public K(int k)
+        {
+            this.k = k;
+        }
+        public void m_k()
+        {
+            Console.WriteLine("Method K, object hash = {0}", this.GetHashCode());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            D objd = new D(1);
+            objd.m_d();
+            E obje = new E(2);
+            obje.m_e();
+            F objf = new F(3);
+            objf.m_f();
+            K objk = new K(4);
+            objk.m_k();
+            J objj = new J(5);
+            objj.m_j();
+            Console.WriteLine("//------------------------");
+            //-------------------------------------------------
+            B objb = new B(objd);
+            objb.m_b();
+            objb.objd.m_d();
+            Console.WriteLine("//------------------------");
+            //-------------------------------------------------
+            C objc = new C(obje, objf, objk);
+            objc.m_c();
+            objc.obje.m_e();
+            objc.objf.m_f();
+            objc.objk.m_k();
+            Console.WriteLine("//------------------------");
+            //-------------------------------------------------
+            //A obja = new A(objb, objc, objj);
+            A obja = new A();
+            B objbb = obja.objb;
+            obja = null;
+
+            obja.m_a();
+            obja.objb.m_b();
+            obja.objb.objd.m_d();
+
+            K objkk = new K(14);
+            C objcc = new C(obje, objf, objkk);
+            objkk = null;
+            objcc.m_c();
+
+        }
+    }
+}
