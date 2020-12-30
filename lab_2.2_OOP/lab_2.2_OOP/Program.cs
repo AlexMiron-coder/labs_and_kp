fb7eaddab6caf2dfe449ba75a3a8ea9a7914c9f2
AlexMiron-coder <57828189+AlexMiron-coder@users.noreply.github.com>
2020-12-30 19:59:51 +0300

    Add files via upload

---
+++ lab_2.2_OOP/lab_2.2_OOP/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace lab_2._2_OOP
+{
+    class A
+    {
+        public B b;
+        private C c;
+        private J j;
+        public A()
+        {
+            b = new B();
+            c = new C();
+            j = new J();
+        }
+        public void m_a()
+        {
+            Console.WriteLine("Method A ");
+        }
+        public class B
+        {
+            private D d;
+            public B()
+            {
+                d = new D();
+            }
+            public void m_b()
+            {
+                Console.WriteLine("Method B, a->b ");
+            }
+            public class D
+            {
+                private int d;
+                public D()
+                {
+                    d = 3;
+                }
+                public void m_d()
+                {
+                    Console.WriteLine("Method D, a->b->d = {0}", d);
+                }
+            }
+        }
+        public class C
+        {
+            private E e;
+            private F f;
+            private K k;
+            public C()
+            {
+                e = new E();
+                f = new F();
+                k = new K();
+            }
+            public void m_c()
+            {
+                Console.WriteLine("Method C, a->c ");
+            }
+            public class E
+            {
+                private int e;
+                public E()
+                {
+                    e = 3;
+                }
+                public void m_e()
+                {
+                    Console.WriteLine("Method E, a->c->e = {0}", e);
+                }
+            }
+            public class F
+            {
+                private int f;
+                public F()
+                {
+                    f = 3;
+                }
+                public void m_f()
+                {
+                    Console.WriteLine("Method F, a->c->f = {0}", f);
+                }
+            }
+            public class K
+            {
+                private int k;
+                public K()
+                {
+                    k = 3;
+                }
+                public void m_k()
+                {
+                    Console.WriteLine("Method K, a->c->k = {0}", k);
+                }
+            }
+        }
+        public class J
+        {
+            private int j;
+            public J()
+            {
+                j = 3;
+            }
+            public void m_j()
+            {
+                Console.WriteLine("Method J, a->j = {0}", j);
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            A.B b = new A.B();
+            A.J j = new A.J();
+            A.B.D d = new A.B.D();
+            A.C.E e = new A.C.E();
+            A.C.F f = new A.C.F();
+            A.C.K k = new A.C.K();
+            A.C c = new A.C();
+            A.B bb = new A.B();
+
+            a.m_a();
+            Console.WriteLine("-----------------------------------------------");
+            b.m_b();
+            d.m_d();
+            Console.WriteLine("-----------------------------------------------");
+            c.m_c();
+            e.m_e();
+            f.m_f();
+            k.m_k();
+            Console.WriteLine("-----------------------------------------------");
+            j.m_j();
+            
+        }
+    }
+}
---
+++ lab_3_OOP/lab_3_OOP/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace lab_3_OOP
+{
+    class F
+    {
+        protected int f = 0;
+        public F() { }
+        public F(int f) { this.f = f; }
+        public F(F objf) { f = objf.f; }
+        public virtual int F1()
+        {
+            return f;
+        }
+        public int get_f { get { return f; } }
+
+    }
+    class D : F
+    {
+        int d;
+        public D() { this.d = 148369; }
+        public D(int d) { this.f = d; }
+        public D(D objd) { f = objd.f; }
+        public override int F1()
+        {
+            return base.F1();
+        }
+        public int get_d { get { return f; } }
+        public int get
+        {
+            get { return d; }
+        }
+    }
+    class G : D
+    {
+        int g;
+        public G(int g) { this.g = g; }
+        public G(int g, int d) : base(d) { this.g = g; Console.WriteLine("hash: {0}", this.GetHashCode()); }
+        public override int F1()
+        {
+            return base.F1();
+        }
+    }
+
+    public class First
+    {
+        protected int a = 0;
+        public virtual void F()
+        {
+            Console.WriteLine("{0}", a);
+        }
+    }
+    public class Second : First
+    {
+        public sealed override void F()
+        {
+            base.F();
+        }
+    }
+    public class Third : Second
+    {
+        public override void F() //Уже нельзя override, так как запечатан
+        {
+            base.F();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F f = new F();
+            F f1 = new F(1);
+            Console.WriteLine("first fucntion working: {0} {1}", f.F1(), f1.F1());
+
+            f = new D(5);
+            f1 = new G(2, 3);
+
+            Console.WriteLine("after base-function working: {0}, {1}", f.F1(), f1.F1());
+
+            D d = new D();
+            f1 = new G(10, 14);
+
+            Console.WriteLine("after second work: {0}, {1}", f1.F1(), d.get);
+
+
+
+        }
+    }
+}
